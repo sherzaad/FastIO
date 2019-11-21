d9729471461a532bb7eef8c2484ab4016330799e
sherzaad <57634578+sherzaad@users.noreply.github.com>
2019-11-21 11:57:01 +0000

    Add files via upload

---
+++ FastIO.h
@@ -0,0 +1,57 @@
+#ifndef	FASTIO_H
+#define	FASTIO_H
+
+#include <arduino.h>
+
+#ifdef ARDUINO_ARCH_AVR
+#define REGTYPE uint8_t   // AVR uses 8-bit registers
+#else
+#define REGTYPE uint32_t
+#endif
+
+struct IO {
+  REGTYPE bitmask;
+  volatile REGTYPE *reg;
+};
+
+inline struct IO set_IO(uint8_t pin, uint8_t io)
+{
+  struct IO temp;
+  REGTYPE port = digitalPinToPort(pin);
+  volatile REGTYPE *mode;
+
+  temp.bitmask = digitalPinToBitMask(pin);
+
+  if (port == NOT_A_PIN) return;
+
+  mode = portModeRegister(port);
+
+  if (io == OUTPUT) {
+    temp.reg = portOutputRegister(port);
+    *mode |= temp.bitmask;
+  }
+  else {
+    *mode &= ~temp.bitmask;
+    temp.reg = portInputRegister(port);
+    //*temp.reg &= ~temp.bitmask;
+    if (io == INPUT_PULLUP)*temp.reg |= temp.bitmask;
+  }
+
+  return temp;
+}
+
+inline bool dig_read(struct IO in) {
+  REGTYPE temp = *in.reg & in.bitmask;
+
+  if (temp) return 1;
+
+  return 0;
+}
+
+inline void dig_write(struct IO out, uint8_t logic) {
+
+  if (logic)  *out.reg |= out.bitmask;
+  else *out.reg &= ~out.bitmask;
+
+}
+#endif
---
+++ Faster_Digital_Read_Write.ino
@@ -0,0 +1,31 @@
+#include "FastIO.h"
+
+struct IO out13;
+struct IO in7;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  out13 = set_IO(13, OUTPUT);
+  in7 = set_IO(7, INPUT_PULLUP);
+
+  Serial.println("READY");
+}
+
+void loop() {
+
+  // blink pin 13, read pin 7
+  //LED ON
+  dig_write(out13, 1);
+  delay(500);
+
+  //LED OFF
+  dig_write(out13, 0);
+  delay(500);
+
+  //print out pin7 state
+  Serial.println(dig_read(in7), DEC);
+
+}
+
